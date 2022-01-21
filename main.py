ca4daa247f16a17c80993ebfa7ea6a64a9074e57
theyeeman <fernando_y767@hotmail.com>
2022-01-21 16:53:15 -0500

    init

--- main.py
+++ main.py
@@ -1,5 +1,15 @@
 import pygame
 from Engine.snake import Snake
+from Engine.screen import Screen
 
 if __name__ == "__main__":
-    print('hello')
+    screen = Screen(20, 20, 10)
+    screen.init_display()
+
+    snake = Snake(screen)
+
+    while not snake.is_dead:
+        # run game
+        pass
+
+    pygame.quit()
