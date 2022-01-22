262c1456584e4477a3997ae798269621487c063e
theyeeman <fernando_y767@hotmail.com>
2022-01-21 21:04:24 -0500

    add WASD control

--- main.py
+++ main.py
@@ -7,6 +7,10 @@ from pygame.locals import (
     K_DOWN,
     K_LEFT,
     K_RIGHT,
+    K_w,
+    K_s,
+    K_a,
+    K_d,
     KEYDOWN,
     KEYUP,
 )
@@ -15,13 +19,13 @@ from pygame.locals import (
 def handle_events():
     for event in pygame.event.get():
         if event.type == KEYDOWN:
-            if event.key == K_UP:
+            if event.key == K_UP or event.key == K_w:
                 snake.key_pressed(Direction.UP)
-            elif event.key == K_RIGHT:
+            elif event.key == K_RIGHT or event.key == K_d:
                 snake.key_pressed(Direction.RIGHT)
-            elif event.key == K_DOWN:
+            elif event.key == K_DOWN or event.key == K_s:
                 snake.key_pressed(Direction.DOWN)
-            elif event.key == K_LEFT:
+            elif event.key == K_LEFT or event.key == K_a:
                 snake.key_pressed(Direction.LEFT)
             break
 
