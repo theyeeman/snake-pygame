b628643b64f66683d00f3b637798b29f274ebe4f
theyeeman <fernando.yee97@gmail.com>
2022-01-24 20:18:10 -0500

    auto format changes

--- Engine/screen.py
+++ Engine/screen.py
@@ -42,7 +42,8 @@ class Screen:
         draw.rect(self.surface, colour, (x_pos, y_pos, self.scale, self.scale))
 
     def draw_info_bar_line(self):
-        draw.line(self.surface, INFO_BAR_LINE_COLOR, (0, self.scale-1), (self.width, self.scale-1), 1)
+        draw.line(self.surface, INFO_BAR_LINE_COLOR,
+                  (0, self.scale-1), (self.width, self.scale-1), 1)
 
     def set_pixel_in_buffer(self, obj, color):
         self._draw_pixel(obj.x, obj.y, color)
@@ -51,7 +52,8 @@ class Screen:
         self._draw_pixel(obj.x, obj.y, BACKGROUND_COLOR)
 
     def draw_wait_init(self):
-        self.text_to_buffer('Press Enter to Start', TEXT_COLOR, self.scale*2, self.width, self.height, centered=True)
+        self.text_to_buffer('Press Enter to Start', TEXT_COLOR,
+                            self.scale*2, self.width, self.height, centered=True)
         self.score_to_buffer(0)
         self.update()
 
@@ -67,23 +69,26 @@ class Screen:
         self.update()
 
     def draw_lose_text(self):
-        self.text_to_buffer('You lost! Press Enter to try again', TEXT_COLOR, self.scale*2, self.width, self.height, centered=True)
+        self.text_to_buffer('You lost! Press Enter to try again', TEXT_COLOR,
+                            self.scale*2, self.width, self.height, centered=True)
         self.update()
 
     def clear_info_bar(self):
-        draw.rect(self.surface, BACKGROUND_COLOR, (0, 0, self.width, self.scale))
+        draw.rect(self.surface, BACKGROUND_COLOR,
+                  (0, 0, self.width, self.scale))
         self.draw_info_bar_line()
 
     def score_to_buffer(self, score):
         self.clear_info_bar()
-        self.text_to_buffer("Score: "+str(score), TEXT_COLOR, self.scale-1, 0, 0)
+        self.text_to_buffer("Score: "+str(score),
+                            TEXT_COLOR, self.scale-1, 0, 0)
 
     def text_to_buffer(self, text, color, size, x, y, centered=False):
-        rendered_text = font.SysFont('arial', size).render(str(text), True, color)
+        rendered_text = font.SysFont(
+            'arial', size).render(str(text), True, color)
 
         if centered:
             text_rect = rendered_text.get_rect(center=(x//2, y//2))
             self.surface.blit(rendered_text, text_rect)
         else:
             self.surface.blit(rendered_text, (x, y))
-        
