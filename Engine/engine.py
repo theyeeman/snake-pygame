15a18701338e6f18be0a5cd8632641e5c2d32625
theyeeman <fernando.yee97@gmail.com>
2022-01-27 21:19:05 -0500

    separate event handling into class

--- Engine/engine.py
+++ Engine/engine.py
@@ -1,20 +1,8 @@
-import queue
 import pygame
-from pygame.locals import (
-    K_UP,
-    K_DOWN,
-    K_LEFT,
-    K_RIGHT,
-    K_w,
-    K_s,
-    K_a,
-    K_d,
-    K_RETURN,
-    KEYDOWN,
-)
+from pygame.locals import K_RETURN, KEYDOWN
 from Engine.snake import Snake, Position
-from Engine.direction import Direction
 from Engine.screen import Screen, SNAKE_COLOR, FOOD_COLOR
+from Engine.eventhandler import EventHandler
 
 
 class Engine():
@@ -23,22 +11,10 @@ class Engine():
         self.start = False
         self.screen = Screen(width, height, scale)
         self.snake = Snake(self.screen)
+        self.event_handler = EventHandler(self.snake)
         self.empty_set = self.generate_empty_set()
         self.food = self.create_food()
-        self.event_queue = queue.SimpleQueue()
         self.clock = pygame.time.Clock()
-        self.valid_keys = set(
-            (
-                K_UP,
-                K_DOWN,
-                K_LEFT,
-                K_RIGHT,
-                K_w,
-                K_s,
-                K_a,
-                K_d,
-            )
-        )
 
         self.screen.draw_wait_init()
 
@@ -65,42 +41,6 @@ class Engine():
 
         return False
 
-    def store_events(self):
-        for event in pygame.event.get():
-            if event.type == KEYDOWN:
-                if event.key in self.valid_keys:
-                    self.event_queue.put(event)
-
-            if event.type == pygame.QUIT:
-                pygame.quit()
-                exit()
-
-    def process_queue_event(self):
-        try:
-            event = self.event_queue.get_nowait()
-
-            if event.key == K_UP or event.key == K_w:
-                self.process_key_press(Direction.UP)
-            elif event.key == K_RIGHT or event.key == K_d:
-                self.process_key_press(Direction.RIGHT)
-            elif event.key == K_DOWN or event.key == K_s:
-                self.process_key_press(Direction.DOWN)
-            elif event.key == K_LEFT or event.key == K_a:
-                self.process_key_press(Direction.LEFT)
-        except queue.Empty:
-            return
-
-    def process_key_press(self, new_direction):
-        head = self.snake.get_head()
-
-        if (head.direction == Direction.DOWN and new_direction == Direction.UP
-                or head.direction == Direction.UP and new_direction == Direction.DOWN
-                or head.direction == Direction.LEFT and new_direction == Direction.RIGHT
-                or head.direction == Direction.RIGHT and new_direction == Direction.LEFT):
-            return
-
-        self.snake.overwrite_head(Position(head.x, head.y, new_direction))
-
     def next_frame(self):
         next_head = self.snake.get_next_head()
 
@@ -126,8 +66,8 @@ class Engine():
         self.screen.update()
 
     def run_loop(self):
-        self.store_events()
-        self.process_queue_event()
+        self.event_handler.store_events()
+        self.event_handler.process_queue_event()
         self.next_frame()
 
     def wait_for_start(self):
@@ -161,4 +101,4 @@ class Engine():
     def reset(self):
         self.start = False
         self.snake = Snake(self.screen)
-        self.event_queue = queue.SimpleQueue()
+        self.event_handler = EventHandler(self.snake)
